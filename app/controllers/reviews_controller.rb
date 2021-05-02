d9b91584769af67f01fd34a80162af5327a1b91f
kaiwa <kj.11235813213455@gmail.com>
2021-05-02 01:56:21 +0000

    tag_nameのエラー

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -6,6 +6,7 @@ class ReviewsController < ApplicationController
   end
   
   def new
+    @tag_list = Tag.all
     @book = Book.find_by(isbn: params[:book_id])
     @review = Review.new
   end
@@ -13,12 +14,16 @@ class ReviewsController < ApplicationController
   def show
     @review = Review.find(params[:id])
     @book = @review.book
+    @review_tags = @review.tags #投稿に紐付けされるタグの取得
   end
 
   def create
+    binding.pry
     @book = Book.find_by(isbn: params[:book_id])
-    @review = current_user.reviews.new(review_params)
+    @review = current_user.reviews.new
+    tag_list = params[:review][:tag_name].split(nil) #split=>送信されてきた値を、スペースで区切って配列化
     if @review.save
+      @review.save_tag(tag_list)
       redirect_to reviews_path(isbn: params[:book_id]), notice: 'レビューに成功しました'
     else
       flash.now[:danger] = 'レビューに失敗しました'
@@ -28,6 +33,6 @@ class ReviewsController < ApplicationController
   
   private
     def review_params
-      params.require(:review).permit(:content, :book_id)
+      params.require(:review).permit(:content, :book_id, :tag_name)
     end
 end
