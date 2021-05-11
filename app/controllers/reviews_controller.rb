70658fe5d2c203b3e7778f069bc4dd02994f0c64
kaiwa <kj.11235813213455@gmail.com>
2021-05-11 14:38:20 +0000

    tag絞り込みできない

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,16 +1,16 @@
 class ReviewsController < ApplicationController
   def index
-    # binding.pry
-    @book = Book.find_by(isbn: params[:book_id]) #createのindexアクションへのリダイレクトでisbnという名前でbook_id渡すためparams[:isbn]
+    @book = Book.find_by(isbn: params[:isbn]) #createのindexアクションへのリダイレクトでisbnという名前でbook_id渡すためparams[:isbn]
     @reviews = Review.all.order(created_at: "DESC") #更新順に表示
     @ranks = Review.find(Like.group(:review_id).order('count(review_id) DESC').pluck(:review_id)) #いいね多い順に表示
+    
     if params[:tag_name]
       @reviews = Review.tagged_with("#{params[:tag_name]}")
     end
+    # binding.pry
   end
   
   def new
-    @tag_list = Tag.all
     @book = Book.find_by(isbn: params[:book_id])
     @review = Review.new
   end
@@ -18,16 +18,13 @@ class ReviewsController < ApplicationController
   def show
     @review = Review.find(params[:id])
     @book = @review.book
-    @review_tags = @review.tags #投稿に紐付けされるタグの取得
   end
 
   def create
-    binding.pry
     @book = Book.find_by(isbn: params[:book_id])
-    @review = current_user.reviews.new
-    tag_list = params[:review][:tag_name].split(nil) #split=>送信されてきた値を、スペースで区切って配列化
+    @review = current_user.reviews.new(review_params)
     if @review.save
-      @review.save_tag(tag_list)
+      # @review.save_tag(tag_list)
       redirect_to reviews_path(isbn: params[:book_id]), notice: 'レビューに成功しました'
     else
       flash.now[:danger] = 'レビューに失敗しました'
