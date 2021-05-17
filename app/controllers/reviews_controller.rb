e35893a33701212da57e2580e4916bc7a2b26b4e
kaiwa <kj.11235813213455@gmail.com>
2021-05-17 09:32:54 +0000

    タグ機能実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
   def index
     @book = Book.find_by(isbn: params[:isbn]) #createのindexアクションへのリダイレクトでisbnという名前でbook_id渡すためparams[:isbn]
     @reviews = Review.all.order(created_at: "DESC") #更新順に表示
-    @ranks = Review.find(Like.group(:review_id).order('count(review_id) DESC').pluck(:review_id)) #いいね多い順に表示
+    @ranks = Review.find(Like.group(:review_id).order("count(review_id) DESC").pluck(:review_id)) #いいね多い順に表示
     
     if params[:tag_name]
       @reviews = Review.tagged_with("#{params[:tag_name]}")
@@ -19,21 +19,22 @@ class ReviewsController < ApplicationController
     @review = Review.find(params[:id])
     @book = @review.book
   end
-
+  
   def create
     @book = Book.find_by(isbn: params[:book_id])
     @review = current_user.reviews.new(review_params)
     if @review.save
       # @review.save_tag(tag_list)
-      redirect_to reviews_path(isbn: params[:book_id]), notice: 'レビューに成功しました'
+      redirect_to reviews_path(isbn: params[:book_id]), notice: "レビューに成功しました"
     else
-      flash.now[:danger] = 'レビューに失敗しました'
+      flash.now[:danger] = "レビューに失敗しました"
       render :new
     end
   end
   
   private
-    def review_params
-      params.require(:review).permit(:content, :book_id, :tag_name)
-    end
+  def review_params
+    params.require(:review).permit(:content, :book_id, :tag_list)
+  end
+  
 end
